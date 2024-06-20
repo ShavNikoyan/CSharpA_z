7acbe0c37931dc8f7c57bf83eee0c30426a0c7b6
ShavNikoyan <ShavNikoyan2006@gmail.com>
2024-06-20 22:14:15 +0400

    Test

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
